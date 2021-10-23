9cc5af31c17740ff50fc57505b68e2267176ea48
soetakara <soetakara.717@gmail.com>
2021-10-23 11:14:48 +0900

    11/14 10:31の修正箇所

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -30,7 +30,7 @@ class PrototypesController < ApplicationController
 
   def update
     if @prototype.update(prototypes_params)
-      redirect_to root_path
+      redirect_to prototype_path(@prototype)
     else
       render :edit
     end
